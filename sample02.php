a735e69021a0ee8d9efec91ca7cdc1be648fceb1
zefar03 <hindmost.theholyone@gmail.com>
2015-05-13 04:36:56 +0000

    add sample01.php

---
+++ sample02.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print('<h2 style="font-size: 100%; font-weight: bold">画面にHTMLを表示する</h2>');
+    print('<p>PHPはHTMLの中に埋め込んで記述することができます。<p>');
+    print('<p>改行するときは、<br />brタグを記述します。</p>');
+    ?>
+</body>
+</html>
---
+++ sample03.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print('現在は ' . date('G時 i分 s秒') . ' デス');
+    ?>
+</body>
+</html>
